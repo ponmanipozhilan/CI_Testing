26c8752d9c13ad362d93c4e976fc127421033050
ponmanipozhilan <ponmanipozhilan@gmail.com>
2017-01-06 14:39:03 +0530

    qq

--- src/CI_Package/Jenkins_Test2.java
+++ src/CI_Package/Jenkins_Test2.java
@@ -22,7 +22,7 @@ public class Jenkins_Test2 {
 	
 	@Test
 	public void printOwnershipCode() throws Exception{
-		String[] commands = {"C:\\Selenium\\Akshay_Simulator\\Debug\\Simulator.exe", "-t lcbs",  "-f"};
+		String[] commands = {"C:\\Akshay_Simulator\\Debug\\Simulator.exe", "-t lcbs",  "-f"};
 		String output1 = getProcessOutput(commands);
 		String code = getOwnershipCode(output1);
 		System.out.println("ownership code received is: ");
